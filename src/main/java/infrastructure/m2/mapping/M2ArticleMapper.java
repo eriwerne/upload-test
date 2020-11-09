4e5e0db4d6376b2ce2d549c43fb2b03f47bb4245
Muharrem Demirci <mdemirci@dm-it.de>
2020-11-09 11:03:10 +0100

    add Blacklist for VariantMirrorInformation

--- src/main/java/infrastructure/m2/mapping/M2ArticleMapper.java
+++ src/main/java/infrastructure/m2/mapping/M2ArticleMapper.java
@@ -10,9 +10,14 @@ import core.article.exceptions.InvalidArticleData;
 import core.article.exceptions.UnknownMirrorInformation;
 import infrastructure.m2.schema.M2ItemData;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class M2ArticleMapper {
+    private final String[] cleanUpMirrorItems = new String[] {
+            "Armlehne",
+            "Bettfunktion"
+    };
+
     public Article mapFromJson(String json) throws RuntimeException, InvalidArticleData {
         M2ItemData data = new Gson().fromJson(json, M2ItemData[].class)[0];
 
@@ -35,6 +40,9 @@ public class M2ArticleMapper {
 
     private String extractMirrorInformationString(M2ItemData data) throws UnknownMirrorInformation {
         String[] variantMirrorInformation = data.getVariantMirrorInformation();
+
+        variantMirrorInformation = cleanUpVariantMirrorInformation(variantMirrorInformation);
+
         if (variantMirrorInformation != null && variantMirrorInformation.length > 1)
             throw new UnknownMirrorInformation(variantMirrorInformation);
 
@@ -45,5 +53,30 @@ public class M2ArticleMapper {
             relevantString = variantMirrorInformation[0];
         }
         return relevantString;
+    }
+
+    private String[] cleanUpVariantMirrorInformation(String[] variantMirrorInformation) {
+        if (variantMirrorInformation == null) {
+            return null;
+        }
+
+        if (variantMirrorInformation.length < 2) {
+            return variantMirrorInformation;
+        }
+
+        String[] newVariantMirrorInformation = Arrays.stream(variantMirrorInformation)
+                .filter(variant -> !existsCleanUpMirrorItem(variant)).toArray(String[]::new);
+
+        return newVariantMirrorInformation;
+    }
+
+    private boolean existsCleanUpMirrorItem(String item) {
+        for (String cleanUpMirrorItem : cleanUpMirrorItems) {
+            if (item.contains(cleanUpMirrorItem)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
